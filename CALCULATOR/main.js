26e7f9511b7dd5af60c905d88ae3f23cbe91d59f
hikmot-kasali <kasalihikmot@gmail.com>
2024-07-20 00:31:28 +0100

    Adding

--- CALCULATOR/main.js
+++ CALCULATOR/main.js
@@ -1,13 +1,4 @@
-let num1 = Number(document.getElementById("one").textContent);
-let num2 = Number(document.getElementById("two").textContent);
-let num3 = Number(document.getElementById("three").textContent);
-let num4 = Number(document.getElementById("four").textContent);
-let num5 = Number(document.getElementById("five").textContent);
-let num6 = Number(document.getElementById("six").textContent);
-let num7 = Number(document.getElementById("seven").textContent);
-let num8 = Number(document.getElementById("eight").textContent);
-let num9 = Number(document.getElementById("nine").textContent);
-let num0 = Number(document.getElementById("zero").textContent);
+
 
 let inputArray = [];
 
